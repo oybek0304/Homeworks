703447d5606d9d4492ab1329ae4321a80c46d80b
Oybek <oybekyoldoshev0304@gmail.com>
2025-06-09 12:17:19 +0500

    Create homework1.py

---
+++ lesson-1/homework/homework1.py
@@ -0,0 +1,29 @@
+# 1 masala
+a = int(input("Enter the side of the Square:"))
+perimeter = 4 * a
+Area = a * a
+print(perimeter)
+print(Area)
+
+# 2 masala
+
+d = int(input("Enter the diameter of the Circle:"))
+Length = 3.14 * d
+print(Length)
+
+# 3 masala
+
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))
+mean = (a+b) / 2 
+print(mean)
+
+# 4 masala
+
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))         
+sum = a + b
+product = a * b
+square_a = a ** 2
+square_b = b ** 2
+print(f"Sum: {sum}, Product: {product}, Square of a: {square_a}, Square of b: {square_b}")
