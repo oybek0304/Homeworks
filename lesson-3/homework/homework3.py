c56623ccc7f5c77dd476855adb8b4f8d1adb1ccf
Oybek <oybekyoldoshev0304@gmail.com>
2025-06-12 16:17:30 +0500

    Create homework3.py

---
+++ lesson-3/homework/homework3.py
@@ -0,0 +1,93 @@
+# 1 masala
+
+fruits = ["banana","cherry ", "apricot", "apple","grape"]
+print(fruits[2])
+
+# 2 masala 
+
+list1 = [1,2,3,4]
+list2 = [5,6,7,8]
+combined = lis1 + list2
+print(combined)
+
+# 3 masala
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+first = numbers[0]
+middle = numbers[len(numbers)// 2]
+last = numbers[-1]
+extract = [first , middle ,last]
+print(extract)
+
+# 4 masala
+
+movies = ["Shawshank Redemption", "Prison Break", "Titanic", "Interstellar", "Inception"]
+movies_tuple = tuple(movies)
+print(movies_tuple)
+
+# 5 masala
+
+cities = ["Berlin", "Tashkent", "Moscow","Paris","London", "New York"]
+print("Paris" in cities)
+
+# 6 masala 
+
+numbers = [1,2,3,4]
+duplicate = numbers * 2
+print(duplicate)
+
+# 7 masala
+
+numbers = [10, 20, 30, 40, 50,60, 70, 80,90,100]
+numbers[0], numbers[-1] = numbers[-1], numbers[0]
+print(numbers)
+
+# 8 masala
+
+numbers = tuple(range(1, 11))
+print(numbers[3:8])
+
+# 9 masala
+
+colors = ["blue", "red", "green", "blue", "yellow", "blue"]
+count_blue = colors.count("blue")
+print(count_blue)
+
+# 10 masala
+
+animals = ("cat", "dog", "lion", "tiger", "elephant","wolf")
+index_lion = animals.index("lion")
+print(index_lion)
+
+# 11 masala
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+merged = tuple1 + tuple2
+print(merged)
+
+# 12 masala
+
+my_list = [1, 2, 3, 4]
+my_tuple = (5, 6, 7)
+print("Length of list:", len(my_list))
+print("Length of tuple:", len(my_tuple))
+
+# 13 masala
+
+num_tuple = (10, 20, 30, 40, 50)
+num_list = list(num_tuple)
+print(num_list)
+
+# 14 masala
+
+numbers = (4, 7, 1, 9, 3)
+print("Max:", max(numbers))
+print("Min:", min(numbers))
+
+# 15 masala
+
+words = ("hello", "world", "python", "rocks", "Star")
+reversed_words = words[::-1]
+print(reversed_words)
+
